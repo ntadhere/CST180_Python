89e820a68b36b3dadbd3bd02c39ae5fe55e5e129
Dao Nguyen <dorothyng284@gmail.com>
2025-05-20 12:43:09 -0700

    tictactoe start

---
+++ doc/topic3/TicTacToe/Cell.py
@@ -0,0 +1,5 @@
+from CellStates import CellStates
+
+class Cell:
+    def __init__(self):
+        self.state = CellStates.OPEN
---
+++ doc/topic3/TicTacToe/CellStates.py
@@ -0,0 +1,7 @@
+from enum import Enum
+
+class CellStates(Enum):
+    OPEN = 0
+    PLAYER1 = 1
+    PLAYER2 = 2
+    COMPUTER = 3
